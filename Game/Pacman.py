f25e83a39962378bc7d5571a089d4974fdc618b2
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-03-16 16:14:13 +0100

    Happy + Unhappy path pacman test

--- Game/Pacman.py
+++ Game/Pacman.py
@@ -1,5 +1,10 @@
 class Pacman:
     def __init__(self, levens, Xcoordinaat, Ycoordinaat, score, kleur, naam):
+        if not isinstance(levens, int):
+            raise ValueError("levens moet een integer zijn")
+
+        if levens < 0:
+            raise ValueError("levens mag niet negatief zijn")
         self.levens = levens
         self.Xcoordinaat = Xcoordinaat
         self.Ycoordinaat = Ycoordinaat
--- Tests/test_pacman.py
+++ Tests/test_pacman.py
@@ -1,10 +1,19 @@
+import pytest
+
 from Game.Pacman import Pacman
 
 
 #Happy Path Test######################################
 
 def test_pacman_initialization():
-    assert False, "Test nog niet in gebruik"
+    pacman = Pacman(3, 0, 0, 0, "geel", "Pacman")
+    assert pacman.levens == 3
+    assert pacman.Xcoordinaat == 0
+    assert pacman.Ycoordinaat == 0
+    assert pacman.score == 0
+    assert pacman.kleur == "geel"
+    assert pacman.naam == "Pacman"
 
 def test_pacman_initialization_unhappy():
-    assert False, "Test nog niet in gebruik"
+    with pytest.raises(ValueError):
+        pacman = Pacman("-1", 0, 0, 0, "geel", "Pacman")
