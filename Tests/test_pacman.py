c8d6d1c953f56d2f18d96870abbce35990ecc6a2
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-03-16 16:05:17 +0100

    unhappy initialization test

--- Tests/test_pacman.py
+++ Tests/test_pacman.py
@@ -1,5 +1,10 @@
 from Game.Pacman import Pacman
 
+
+#Happy Path Test######################################
+
 def test_pacman_initialization():
     assert False, "Test nog niet in gebruik"
 
+def test_pacman_initialization_unhappy():
+    assert False, "Test nog niet in gebruik"
