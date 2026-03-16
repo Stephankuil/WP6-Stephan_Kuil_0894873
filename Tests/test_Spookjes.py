681e3254de116b2ce2c5fec7e1914814dc03f26e
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-03-16 16:27:21 +0100

    Test voor spookjes. happy path

--- Tests/test_Spookjes.py
+++ Tests/test_Spookjes.py
@@ -5,4 +5,12 @@ from Game.Spookjes import Spookjes
 #Happy Path Test######################################
 
 def test_spookjes_initialization():
-    assert False
+    spookjes = Spookjes(4, "Blinky", "rood", 5, 5, True)
+    assert spookjes.aantal_spookjes == 4
+    assert spookjes.naam == "Blinky"
+    assert spookjes.kleur == "rood"
+    assert spookjes.x_coordinaat == 5
+    assert spookjes.y_coordinaat == 5
+    assert spookjes.opeetbaar == True
+
+
