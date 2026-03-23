84ed914d68c028f95869d20c2159c84dd383f294
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-03-23 15:32:04 +0100

    test voor Kaasjes unhappy path

--- Tests/test_Kaas.py
+++ Tests/test_Kaas.py
@@ -10,3 +10,13 @@ def test_kaas_eet():
     assert kaas.eet_kaas(1, 1) is True
     assert kaas.eet_kaas(1, 1) is False
     assert kaas.aantal_over() == 1
+
+
+
+#unhappy path test######################################
+
+def test_kaas_eet_unhappy():
+    kaas = Kaas([(1, 1), (2, 2)])
+
+    assert kaas.eet_kaas(3, 3) is False
+    assert kaas.aantal_over() == 2
