2fbdb9ea25b5c2202c75012a3985b9de5379fe41
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-03-26 15:09:33 +0100

    game window basis compleet

--- Game/Game_window.py
+++ Game/Game_window.py
@@ -1,17 +1,23 @@
 import pygame
 from sys import exit
 
-
+game_breedte = 800
+game_hoogte = 600
 pygame.init()
 
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((game_breedte, game_hoogte))
 
 pygame.display.set_caption("Pacman")
-
+klok = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    pygame.display.update()
+    klok.tick(60)
+
+
 
